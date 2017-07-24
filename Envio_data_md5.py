a1c8087ca284ef015862f4c86c05f3e74d998c15
romel <lssromel@hotmail.com>
2017-07-24 03:56:53 -0500

    cambio ip

--- Envio_data_md5.py
+++ Envio_data_md5.py
@@ -3,15 +3,15 @@ from login_logout import *
 import hashlib
 
 session = requests.sessions.Session()
-url_in='http://34.205.92.167:5000/login_user/'
+url_in='http://0.0.0.0:5000/login_user/'
 
 payload = {'username': 'renting', 'password': 'colombia'}
 
-url_out='http://34.205.92.167:5000/logout_user'
+url_out='http://0.0.0.0:5000/logout_user'
 
 session = login_fun(session,url_in,payload)
 
-ruta = '/Users/lssro/cliente-app/viajes.xlsx.zip'
+ruta = '/home/romel/Api_Cliente/viajes.xlsx.zip'
 f= open(ruta, 'rb')
 
 md5= hashlib.md5(f.read()).hexdigest()
@@ -19,7 +19,7 @@ f= open(ruta, 'rb')
 
 token=session.cookies['csrftoken']
 
-r =session.post('http://34.205.92.167:5000/send_data',files = {'file':f}, data={'csrfmiddlewaretoken': token,"MD5":md5,"name":"viajes"})
+r =session.post('http://0.0.0.0:5000/send_data',files = {'file':f}, data={'csrfmiddlewaretoken': token,"MD5":md5,"name":"viajes"})
 print r, r.text
 
 
